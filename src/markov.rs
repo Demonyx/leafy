ad7a2dbe0897a0f92f65aab7976f8f25361552c2
Nicola Vermeulen <nicola@lolwut.co.za>
2023-06-19 15:07:22 +0200

    Add markov name generator

--- src/markov.rs
+++ src/markov.rs
@@ -1,4 +1,6 @@
+use rand::seq::SliceRandom;
 use serde::{Deserialize, Serialize};
+use serde_json::Result;
 use std::{
     collections::HashMap,
     fs::File,
@@ -14,7 +16,7 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-pub fn train(filenames: Vec<String>) -> Result<(), Error> {
+pub fn train(filenames: Vec<String>) {
     let mut corpus = Vec::new();
     let mut model: HashMap<String, HashMap<String, i32>> = HashMap::new();
     for file in filenames {
@@ -49,12 +51,24 @@ pub fn train(filenames: Vec<String>) -> Result<(), Error> {
             .or_insert(HashMap::from([("END".to_string(), 1)]));
     }
     // temporary until db is set up
-    let mut file = File::create("../corpus/model")?;
+    let mut file = File::create("../corpus/model").expect("Cound not open file");
     let model_json = serde_json::to_string(&model).unwrap();
-    file.write_all(model_json.as_bytes())
+    file.write_all(model_json.as_bytes()).unwrap();
 }
 
-pub fn generate(num: i32) {
+fn find_next_token(counts: HashMap<String, i32>) -> String {
+    let mut options = Vec::new();
+    for token in counts.keys() {
+        let mut i = *counts.get(token).unwrap();
+        while i > 0 {
+            options.push(token);
+            i -= 1;
+        }
+    }
+    options.choose(&mut rand::thread_rng()).unwrap().to_string()
+}
+
+pub fn generate(num: i32) -> Vec<String> {
     // temporary until db is set up
     let file = File::open("../corpus/model").expect("No such file");
     let mut buf_reader = BufReader::new(file);
@@ -63,5 +77,20 @@ pub fn generate(num: i32) {
         .read_to_string(&mut contents)
         .expect("Could not read file");
 
-    print!("{}", contents);
+    let data: HashMap<String, HashMap<String, i32>> =
+        serde_json::from_str(&contents).expect("Error converting to JSON");
+
+    let mut names = Vec::new();
+    for n in 0..num {
+        let mut current_token = "START".to_string();
+        let mut new_name = String::new();
+        while current_token != "END" {
+            new_name.push_str(&current_token);
+            current_token = find_next_token(data.get(&current_token).unwrap().clone());
+        }
+        new_name = new_name[5..].to_string();
+        // println!("{}", new_name);
+        names.push(new_name);
+    }
+    names
 }
