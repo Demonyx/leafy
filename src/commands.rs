2374e8c6f274b96a9ddb084feb65cd6f0c012a57
Nicola Vermeulen <nicola@lolwut.co.za>
2023-07-06 15:50:39 +0200

    Add reactions to names list

--- src/commands.rs
+++ src/commands.rs
@@ -11,13 +11,23 @@ pub async fn names(
 ) -> Result<(), Error> {
     let name_list = markov::generate(5);
     let mut response = String::new();
-    let mut i = 1;
+    response.push_str("Vote for the name for your plant: \n");
+    // let reacts = ['🌲', '🌱', '🌵', '🍀', '🌿'];
+    let reacts = ['🌻', '🌹', '🌷', '💐', '🌸'];
+
+    let mut i = 0;
     for name in name_list {
-        response.push_str(&format!("{}. ", i));
+        response.push_str(&format!("{} ", reacts[i]));
         response.push_str(&name);
         response.push_str("\n");
         i += 1;
     }
-    ctx.say(response).await?;
+
+    let reply_handle = ctx.send(|message| message.content(response)).await?;
+    let message = reply_handle.message().await?;
+    for emoji in reacts {
+        message.react(ctx.serenity_context(), emoji).await?;
+    }
+
     Ok(())
 }
